0488be5496e36a52b86daf8e3fc0fc6be1984c91
jmayta1984 <jmayta@pucp.edu.pe>
2025-09-15 08:40:28 -0500

    fix: organize imports

--- EasyShop/app/src/main/java/pe/edu/upc/easyshop/core/ui/components/ProductCard.kt
+++ EasyShop/app/src/main/java/pe/edu/upc/easyshop/core/ui/components/ProductCard.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
