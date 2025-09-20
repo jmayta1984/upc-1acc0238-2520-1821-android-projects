0daf6c6d64811adfe00a130d1b3ec21e68a7e257
jmayta1984 <jmayta@pucp.edu.pe>
2025-09-20 08:15:15 -0500

    feat (home): add dependencies

---
+++ EasyShop/app/src/main/java/pe/edu/upc/easyshop/features/home/data/di/DataModule.kt
@@ -0,0 +1,25 @@
+package pe.edu.upc.easyshop.features.home.data.di
+
+import pe.edu.upc.easyshop.features.home.data.repositories.ProductRepositoryImpl
+import pe.edu.upc.easyshop.features.home.data.services.ProductService
+import pe.edu.upc.easyshop.features.home.domain.repositories.ProductRepository
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object DataModule {
+
+    fun getProductRepository(): ProductRepository {
+        return ProductRepositoryImpl(getProductService())
+    }
+
+    fun getProductService(): ProductService {
+        return getRetrofit().create(ProductService::class.java)
+    }
+
+    fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://dummyjson.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
