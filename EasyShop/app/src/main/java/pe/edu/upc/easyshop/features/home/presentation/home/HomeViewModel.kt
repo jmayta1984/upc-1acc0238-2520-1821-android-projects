0014064ff75b1c826eb13f4e734372c095eb89d2
jmayta1984 <jmayta@pucp.edu.pe>
2025-09-20 08:15:25 -0500

    feat (home): add viewmodel

---
+++ EasyShop/app/src/main/java/pe/edu/upc/easyshop/features/home/presentation/home/HomeViewModel.kt
@@ -0,0 +1,28 @@
+package pe.edu.upc.easyshop.features.home.presentation.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import pe.edu.upc.easyshop.features.home.domain.repositories.ProductRepository
+import pe.edu.upc.easyshop.shared.models.Product
+
+class HomeViewModel(private val repository: ProductRepository): ViewModel() {
+    private val _products = MutableStateFlow(emptyList<Product>())
+    val products: StateFlow<List<Product>> = _products
+
+    fun getAllProducts() {
+        viewModelScope.launch {
+            _products.value = repository.getAllProducts()
+
+        }
+
+    }
+
+    init {
+        getAllProducts()
+    }
+
+}
