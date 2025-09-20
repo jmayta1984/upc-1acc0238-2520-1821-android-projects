097a2a3f5c425db82dbc79c5b13323a0612c7f11
jmayta1984 <jmayta@pucp.edu.pe>
2025-09-20 08:15:41 -0500

    feat (home): add repository

---
+++ EasyShop/app/src/main/java/pe/edu/upc/easyshop/features/home/data/repositories/ProductRepositoryImpl.kt
@@ -0,0 +1,30 @@
+package pe.edu.upc.easyshop.features.home.data.repositories
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import pe.edu.upc.easyshop.features.home.data.services.ProductService
+import pe.edu.upc.easyshop.features.home.domain.repositories.ProductRepository
+import pe.edu.upc.easyshop.shared.models.Product
+
+class ProductRepositoryImpl(private val service: ProductService): ProductRepository {
+    override suspend fun getAllProducts(): List<Product> = withContext(Dispatchers.IO) {
+
+        val response = service.getAllProducts()
+
+        if (response.isSuccessful) {
+            response.body()?.let { productsWrapperDto ->
+                productsWrapperDto.products?.let { productsDto ->
+                    return@withContext productsDto.map { dto ->
+                        Product(name = dto.title ?: "",
+                            price = dto.price ?: 0.0,
+                            description = dto.description ?: "",
+                            image = dto.thumbnail ?: "")
+                    }
+                }
+
+            }
+        }
+
+        return@withContext emptyList()
+    }
+}
