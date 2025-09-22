582ecbf0c088c2e876558b97fe3bb9498e46a44f
jmayta1984 <jmayta@pucp.edu.pe>
2025-09-22 10:58:47 -0500

    feat (home): add entity model & dao

---
+++ EasyShop/app/src/main/java/pe/edu/upc/easyshop/features/home/data/local/dao/ProductDao.kt
@@ -0,0 +1,25 @@
+package pe.edu.upc.easyshop.features.home.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import pe.edu.upc.easyshop.features.home.data.local.models.ProductEntity
+
+@Dao
+interface ProductDao {
+
+    @Insert
+    suspend fun insert(vararg entity: ProductEntity)
+
+    @Delete
+    suspend fun delete(vararg entity: ProductEntity)
+
+    @Update
+    suspend fun update(vararg entity: ProductEntity)
+
+    @Query("select * from product")
+    suspend fun fetchAll(): List<ProductEntity>
+
+}
---
+++ EasyShop/app/src/main/java/pe/edu/upc/easyshop/features/home/data/local/models/ProductEntity.kt
@@ -0,0 +1,13 @@
+package pe.edu.upc.easyshop.features.home.data.local.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "product")
+data class ProductEntity(
+    @PrimaryKey
+    val id: Int,
+    val name: String,
+    val description: String,
+    val image: String
+)
